88f801bf8b8cdedf7a150928879ef2c281e43fcf
Dsfr62 <saviodudu79@gmail.com>
2023-03-21 08:18:41 -0300

    Finalizado

---
+++ main.py
@@ -0,0 +1,19 @@
+# Nome: Bruno Lago      - RM86810
+# Nome: Eduardo Kentho  - RM551971
+# Nome: Kayke Andrade   - RM550738
+# Nome: Eduardo Sávio   - RM97833
+
+tempo_anos = float(input("Há quanto tempo você fuma? (anos)....: "))
+
+valor_maco = float(input("Qual o valor atual de um maço de cigarro?....: "))
+quantidade_dia = float(input("Qual a quantidade de maços, em média, que você fuma por dia?....: "))
+
+tempo_dias = (tempo_anos * 12) * 30
+gasto_total = (valor_maco * quantidade_dia) * tempo_dias
+
+if(gasto_total < 20000):
+  print(f"Com o valor R${gasto_total}, você poderia ter dado entrada em um carro.")
+elif(20000 < gasto_total < 50000):
+  print(f"Com o valor R${gasto_total}, você poderia ter comprado um carro popular usado.")
+else:
+  print(f"Com o valor R${gasto_total}, você poderia ter comprado um carro zero.")
